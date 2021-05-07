b9a0b0b1c7a3014d8e73021d1861c49705bcd59d
aum-khatlawala <83028359+aum-khatlawala@users.noreply.github.com>
2021-05-07 19:25:36 +0400

    Update main.c

--- main.c
+++ main.c
@@ -1,9 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+
+///////////////
+/*
+This is the main driver function for the code which takes in string arguments from the command line interface and performs the required task
+*/
+//////////////
+
 int main()
 {
     graph Graph[101];
+    /*
+    The program keeps running until it is explicity instructed to "End"
+    */
     while (1)
     {
         char op[10];
@@ -16,13 +26,13 @@ int main()
             {
                 char bank[100];
                 scanf("%s", bank);
-                long long int hashval = 0, i = 0;
-                while (bank[i] != '\0')
-                {
-                    hashval = (37 * hashval) + (int)bank[i++];
-                }
-                hashval = hashval % 101;
-                Initialise(Graph[hashval]);
+//                 long long int hashval = 0, i = 0;
+//                 while (bank[i] != '\0')
+//                 {
+//                     hashval = (37 * hashval) + (int)bank[i++];
+//                 }
+//                 hashval = hashval % 101;
+                Initialise(Graph);
             }
             if (strcmp(arr, "currency") == 0)
             {
@@ -34,14 +44,14 @@ int main()
                 scanf("%s", to);
                 int cost;
                 scanf("%d", &cost);
-                long long int hashval = 0, i = 0;
-                while (bank[i] != '\0')
-                {
-                    hashval = (37 * hashval) + (int)bank[i++];
-                }
-                hashval = hashval % 101;
-                // InsertInGraph (bankname, curr_a, curr_b, weight)
-                InsertInGraph(Graph[hashval], from, to, cost);
+//                 long long int hashval = 0, i = 0;
+//                 while (bank[i] != '\0')
+//                 {
+//                     hashval = (37 * hashval) + (int)bank[i++];
+//                 }
+//                 hashval = hashval % 101;
+//                 // InsertInGraph (bankname, curr_a, curr_b, weight)
+                InsertInGraph(Graph, from, to, cost);
             }
         }
         if (strcmp(op, "Delete") == 0)
@@ -51,13 +61,13 @@ int main()
             {
                 char bank[100];
                 scanf("%s", bank);
-                long long int hashval = 0, i = 0;
-                while (bank[i] != '\0')
-                {
-                    hashval = (37 * hashval) + (int)bank[i++];
-                }
-                hashval = hashval % 101;
-                DeleteGraph(Graph[hashval]);
+//                 long long int hashval = 0, i = 0;
+//                 while (bank[i] != '\0')
+//                 {
+//                     hashval = (37 * hashval) + (int)bank[i++];
+//                 }
+//                 hashval = hashval % 101;
+                DeleteGraph(Graph);
             }
             if (strcmp(arr, "currency") == 0)
             {
@@ -67,13 +77,13 @@ int main()
                 char to[20];
                 scanf("%s", from);
                 scanf("%s", to);
-                long long int hashval = 0, i = 0;
-                while (bank[i] != '\0')
-                {
-                    hashval = (37 * hashval) + (int)bank[i++];
-                }
-                hashval = hashval % 101;
-                DeleteNodeInGraph(Graph[hashval], from, to);
+//                 long long int hashval = 0, i = 0;
+//                 while (bank[i] != '\0')
+//                 {
+//                     hashval = (37 * hashval) + (int)bank[i++];
+//                 }
+//                 hashval = hashval % 101;
+                DeleteNodeInGraph(Graph, from, to);
             }
         }
         if (strcmp(op, "End") == 0)
